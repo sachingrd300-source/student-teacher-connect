58d6a21e5114c4028fe92f7252b2c9325bb1cfc0
SACHIN Pandit <sachingrd300@gmail.com>
2026-01-25 18:06:10 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:751471538706:web:3c1a730e676c276e82decb",
   "apiKey": "AIzaSyC0TkOUldYxKZw8ONktALH-5Pc_rnGriSk",
   "authDomain": "studio-182085797-c7d81.firebaseapp.com",
+  "storageBucket": "studio-182085797-c7d81.appspot.com",
   "measurementId": "G-BZ6SD20FZJ",
   "messagingSenderId": "751471538706"
 };
